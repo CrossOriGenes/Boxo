922fc047bcb05a6847a87e991c13603a0ab53bf1
CrossOriGenes <crossorigenes@gmail.com>
2026-08-09 23:36:54 +0530

    Level complete dialog overlay UI completed

---
+++ Assets/Scripts/LevelCompletedUI.cs
@@ -0,0 +1,193 @@
+using DG.Tweening;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelCompletedUI : MonoBehaviour
+{
+    [Header("Panel & Title")]
+    [SerializeField] private RectTransform panel;
+    [SerializeField] private RectTransform title;
+
+    [Header("Stars")]
+    [SerializeField] private Image[] stars;
+    
+    [Header("Texts")]
+    [SerializeField] private TMP_Text scoreText;
+    [SerializeField] private TMP_Text xpText;
+    [SerializeField] private TMP_Text starText;
+    [SerializeField] private TMP_Text healthText;
+    [SerializeField] private TMP_Text reviveText;
+    [SerializeField] private TMP_Text accuracyText;
+
+    [Header("Button")]
+    [SerializeField] private RectTransform confirmButton;
+
+    private CanvasGroup _canvasGroup;
+    
+    private void Awake()
+    {
+        _canvasGroup = gameObject.GetComponent<CanvasGroup>();
+
+        scoreText.text = "0%";
+        xpText.text = "0";
+        starText.text = "0";
+        healthText.text = "0%";
+        reviveText.text = "0";
+        accuracyText.text = "0%";
+    }
+
+    public void Show(
+        int score,
+        int starsEarned,
+        int xp,
+        int health,
+        int revives,
+        float accuracy
+        )
+    {
+        _canvasGroup
+            .DOFade(1f, .45f)
+            .SetEase(Ease.OutQuad)
+            .SetUpdate(true);
+        
+        Sequence sequence = DOTween.Sequence().SetUpdate(true);
+        sequence.Append(
+            panel.DOScale(1f, .35f)
+            .SetEase(Ease.OutBack)
+        );
+        sequence.Join(
+            title.DOAnchorPosX(0, .65f)
+            .SetEase(Ease.OutCubic)
+        );
+        sequence.AppendCallback(() =>
+        {
+            AnimateNumbers(
+                score,
+                starsEarned,
+                xp,
+                health,
+                revives,
+                accuracy
+            );
+            AnimateStars(accuracy);
+        });
+        sequence.AppendInterval(.45f);
+        sequence.Append(
+            confirmButton
+            .DOAnchorPosY(-190, .35f)
+            .SetEase(Ease.OutBounce)
+        );
+        sequence.AppendCallback(() =>
+        {
+            _canvasGroup.interactable = true;
+            _canvasGroup.blocksRaycasts = true;
+        });
+    }
+
+    private void AnimateNumbers(
+        int score,
+        int starsEarned,
+        int xp,
+        int health,
+        int revives,
+        float accuracy
+    )
+    {
+        DOTween.To(
+            () => 0,
+            value =>
+            {
+                scoreText.text = value.ToString();
+            },
+            score,
+            .8f
+        ).SetUpdate(true);
+        DOTween.To(
+            () => 0,
+            value =>
+            {
+                starText.text = value.ToString();
+            },
+            starsEarned,
+            .8f
+        ).SetUpdate(true);
+        DOTween.To(
+            () => 0,
+            value =>
+            {
+                xpText.text = value.ToString();
+            },
+            xp,
+            .8f
+        ).SetUpdate(true);
+        DOTween.To(
+            () => 0,
+            value =>
+            {
+                healthText.text = value + "%";
+            },
+            health,
+            .8f
+        ).SetUpdate(true);
+        DOTween.To(
+            () => 0,
+            value =>
+            {
+                reviveText.text = value.ToString();
+            },
+            revives,
+            .8f
+        ).SetUpdate(true);
+        DOTween.To(
+            () => 0,
+            value =>
+            {
+                accuracyText.text = value + "%";
+            },
+            accuracy,
+            .8f
+        ).SetUpdate(true);
+    }
+
+    private void AnimateStars(float accuracy)
+    {
+        int count = CalculateStars(accuracy);
+
+        Sequence seq = DOTween.Sequence().SetUpdate(true);
+
+        for (int i = 0; i < count; i++)
+        {
+            int index = i;
+
+            Image starFill = stars[index];
+            RectTransform starRoot = starFill.transform.parent as RectTransform;
+
+            seq.AppendInterval(.15f);
+            seq.Append(
+                starFill
+                    .DOFillAmount(1f, .25f)
+                    .SetEase(Ease.OutQuad)
+            );
+            seq.Join(
+                starRoot
+                    .DOScale(1.15f, .18f)
+                    .SetEase(Ease.OutQuad)
+                    .SetLoops(2, LoopType.Yoyo)
+            );
+            // seq.AppendInterval(.15f);
+            // seq.AppendCallback(
+            //     () => PlayStarSound(index)
+            // );
+        }
+    }
+
+    private int CalculateStars(float accuracy)
+    {
+        if (accuracy >= 90) return 3;
+        if (accuracy >= 70) return 2;
+        if (accuracy >= 45) return 1;
+
+        return 0;
+    }
+}
--- Assets/Scripts/LevelExitPortal.cs
+++ Assets/Scripts/LevelExitPortal.cs
@@ -8,6 +8,7 @@ public class LevelExitPortal : MonoBehaviour
     [Header("Fields")]
     [SerializeField] private Transform _centerPoint;
     [SerializeField] private Light2D _globalLight;
+    [SerializeField] private LevelCompletedUI _levelCompletedUI;
 
     private bool _entered;
     private Rigidbody2D _rb;
@@ -49,22 +50,40 @@ public class LevelExitPortal : MonoBehaviour
 
     private IEnumerator ShowLevelCompletedScreen()
     {
-        DOTween.To(
-            () => _globalLight.intensity,
-            x => _globalLight.intensity = x,
-            0f,
-            0.7f
-        )
-        .SetEase(Ease.OutQuad);
-        DOTween.To(
-            () => _playerLight.intensity,
-            x => _playerLight.intensity = x,
-            0f,
-            .45f
-        )
-        .SetEase(Ease.InQuad);
+        yield return new WaitForSeconds(0.15f);
 
-        yield return new WaitForSeconds(0.75f);
-        Debug.Log("Level Completed");
+        Sequence fadeSequence = DOTween.Sequence();
+        fadeSequence.Join(
+            DOTween.To(
+                () => _globalLight.intensity,
+                x => _globalLight.intensity = x,
+                0f,
+                0.7f
+            )
+            .SetEase(Ease.OutQuad)
+        );
+        fadeSequence.Join(
+            DOTween.To(
+                () => _playerLight.intensity,
+                x => _playerLight.intensity = x,
+                0f,
+                .45f
+            )
+            .SetEase(Ease.InQuad)
+        );
+
+        yield return fadeSequence.WaitForCompletion();
+        yield return new WaitForSeconds(.15f);
+
+        _levelCompletedUI.Show(
+            score: 32,
+            starsEarned: 5,
+            xp: 324,
+            health: 100,
+            revives: 2,
+            accuracy: 92f
+        );
+
+        Time.timeScale = 0;
     }
 }
